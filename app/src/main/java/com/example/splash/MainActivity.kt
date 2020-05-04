05518136ec36f1615bf546049bbf1e71eaf276ec
shaheemshanpp <46243023+shaheemshanpp@users.noreply.github.com>
2020-05-04 12:52:00 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/splash/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("https://shaheem-pp.netlify.app")
+        webView.settings.javaScriptEnabled = true
+
+    }
+}
